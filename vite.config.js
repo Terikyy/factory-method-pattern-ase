5298a51e58e61268d56c9683e791b7978846be8f
Terik <75367798+Terikyy@users.noreply.github.com>
2025-12-03 14:57:14 +0100

    Add Vite build tooling and dev server

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '/src': resolve(__dirname, './src'),
+    },
+  },
+});
